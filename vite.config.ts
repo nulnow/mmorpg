7e876f87ac758cf10d502857516f141bc7d2a00e
Разуваев Андрей <andrey.razuvaev@gazprombank.ru>
2023-06-07 18:13:26 +0300

    mobile support fix 4

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   base: '',
   build: {
     minify: false,
+    outDir: './docs',
     rollupOptions: {
       output: {
         assetFileNames: `assets/[name].[ext]`
